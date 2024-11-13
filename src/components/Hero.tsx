85a96e11538dff4402dc23fefb0cfa87b4376137
Bhuwan Acharya Upadhyaya <v1acharya34@gmail.com>
2024-11-13 14:30:00 +0545

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import ContainerSection from './partials/ContainerSection'
 
 export const heroContent = {
     title: "World's Petition Platform",
-    description: "We started in 2016 with the radical idea that anyone, anywhere, should be able to easily and securely to start their own petition. Today, we offer a trusted and easy-to-use platform for social movement across the world."
+    description: "We started in 2015 with the radical idea that anyone, anywhere, should be able to easily and securely to start their own petition. Today, we offer a trusted and easy-to-use platform for social movement across the world."
 }
 
 function Hero() {
